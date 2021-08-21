e59b6da9275e5ac67f583955ebd35be0fee5dff3
1prime1 <87894324+1prime1@users.noreply.github.com>
2021-08-21 22:25:58 +0530

    s2: bliss adaption

--- bliss_s2.mk
+++ bliss_s2.mk
@@ -14,6 +14,10 @@
 # limitations under the License.
 #
 
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
