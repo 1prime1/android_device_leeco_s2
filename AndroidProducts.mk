3c5b22a029fa5066709301c497f770ee92c3736a
1prime1 <87894324+1prime1@users.noreply.github.com>
2021-08-28 18:51:41 +0530

    s2: Bliss adption

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_s2.mk
+    $(LOCAL_DIR)/bliss_s2.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_s2-user \
-    lineage_s2-userdebug \
-    lineage_s2-eng
+    bliss_s2-user \
+    bliss_s2-userdebug \
+    bliss_s2-eng
